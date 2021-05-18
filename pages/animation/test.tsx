9486bf704e9662e3127596d3a3fccdf4c989ce27
parkoon <devparkoon@gmail.com>
2021-05-18 16:32:11 +0900

    feat: 프로젝트 세팅

---
+++ pages/animation/test.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Text() {
+  return <div>Test Page</div>
+}
+
+export default Text
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <div onClick={() => alert('zz')}>
+        <iframe src="/animation/test" />
+      </div>
+    </div>
+  )
+}
